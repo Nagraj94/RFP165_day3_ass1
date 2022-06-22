6241d4e9dfbf9ea1602b92c68f85808b0eb207de
Ngowda94 <Nagraj94>
2022-06-22 22:15:37 +0530

    added daily employee wages

--- src/com/bridgelabz/UC1.java
+++ src/com/bridgelabz/UC1.java
@@ -3,12 +3,21 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class UC1 {
+    static final int EMP_IS_FULL_TIME = 1;
+    static final int WAGES_PER_HRS = 20;
+
+
     public static void main(String[] args) {
-        int EMP_IS_FULL_TIME = 1;
+        int empHrs = 0;
+        int empWages = 0;
+
         double empCheck = Math.floor(Math.random()*10) % 2;
-        if(EMP_IS_FULL_TIME == empCheck)
+        if(empCheck == EMP_IS_FULL_TIME )
         {
+            empHrs =8;
             System.out.println("employee is present");
+            empWages = WAGES_PER_HRS * empHrs;
+            System.out.println("daily wages of employee is: " + empWages);
         }
         else
         {
