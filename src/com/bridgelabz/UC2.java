e4d77ed338ea36b68c34d2016e815542f3baaa23
Ngowda94 <Nagraj94>
2022-06-22 09:25:12 +0530

    Daily employee wages

---
+++ src/com/bridgelabz/UC2.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class UC2 {
+    public static void main(String[] args) {
+        int WAGES_PER_HRS = 20;
+        int FULL_DAY_HRS = 8;
+
+        int dailyWages = WAGES_PER_HRS * FULL_DAY_HRS;
+        System.out.println("employee daily wages is: " + dailyWages);
+    }
+}
