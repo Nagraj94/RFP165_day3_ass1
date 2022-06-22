f8618e21e1e480ac090d4cc67fe34e211b5949ae
Ngowda94 <Nagraj94>
2022-06-22 21:18:09 +0530

    checking for employee presence

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -1,12 +1,11 @@
 package com.bridgelabz;
 
-import java.util.Random;
+public class EmpWages {
 
-public class UC1 {
     public static void main(String[] args) {
         int EMP_IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random()*10) % 2;
-        if(EMP_IS_FULL_TIME == empCheck)
+        int empCheck = (int)Math.floor(Math.random()*10)%2;
+        if (empCheck==EMP_IS_FULL_TIME)
         {
             System.out.println("employee is present");
         }
