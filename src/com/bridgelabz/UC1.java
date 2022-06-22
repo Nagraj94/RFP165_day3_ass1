23908bdf2b9c606dfc1fa360f668eb633b540096
Ngowda94 <Nagraj94>
2022-06-22 22:43:40 +0530

    using switch case

--- src/com/bridgelabz/UC1.java
+++ src/com/bridgelabz/UC1.java
@@ -3,16 +3,30 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class UC1 {
+    static final int EMP_IS_FULL_TIME = 1;
+    static final int EMP_IS_PART_TIME = 2;
+    static final int WAGES_PER_HRS = 20;
+
+
     public static void main(String[] args) {
-        int EMP_IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random()*10) % 2;
-        if(EMP_IS_FULL_TIME == empCheck)
-        {
-            System.out.println("employee is present");
-        }
-        else
-        {
-            System.out.println("employee is absent");
-        }
+        int empHrs = 0;
+        int empWages = 0;
+
+        int empCheck = (int) Math.floor(Math.random()*10) % 3;
+       switch ((int) empCheck){
+           case EMP_IS_FULL_TIME :
+               empHrs = 8;
+               System.out.println("employee is full timer");
+               break;
+           case EMP_IS_PART_TIME:
+               empHrs = 4;
+               System.out.println("employee is part timer");
+               break;
+           default:
+               empHrs = empHrs;
+               System.out.println("employee is absent");
+       }
+       empWages = empHrs * WAGES_PER_HRS;
+        System.out.println("daily employee wages is " + empWages);
     }
 }
