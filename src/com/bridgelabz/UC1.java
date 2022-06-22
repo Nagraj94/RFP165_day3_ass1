00df2bb71aafcae96545ab87ae131ca5b4b0b9ae
Ngowda94 <Nagraj94>
2022-06-22 22:32:18 +0530

    added part time employee and wages

--- src/com/bridgelabz/UC1.java
+++ src/com/bridgelabz/UC1.java
@@ -3,14 +3,30 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class UC1 {
+    static final int EMP_IS_FULL_TIME = 1;
+    static final int EMP_IS_PART_TIME = 2;
+    static final int WAGES_PER_HRS = 20;
+
+
     public static void main(String[] args) {
-        int EMP_IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random()*10) % 2;
-        if(EMP_IS_FULL_TIME == empCheck)
+        int empHrs = 0;
+        int empWages = 0;
+
+        double empCheck = Math.floor(Math.random()*10) % 3;
+        if(empCheck == EMP_IS_FULL_TIME )
         {
-            System.out.println("employee is present");
+            empHrs =8;
+            System.out.println("Full time employee is present");
+            empWages = WAGES_PER_HRS * empHrs;
+            System.out.println("daily wages of employee is: " + empWages);
         }
-        else
+        else if (empCheck == EMP_IS_PART_TIME)
+        {
+            empHrs =4;
+            System.out.println("Part time employee is present");
+            empWages = WAGES_PER_HRS * empHrs;
+            System.out.println("daily wages of employee is: " + empWages);
+        } else
         {
             System.out.println("employee is absent");
         }
